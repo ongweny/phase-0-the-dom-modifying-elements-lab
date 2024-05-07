ceedfa590338a55950d1ea58e0b99558dbcd9df1
ongweny <ongwenyjoel@gmail.com>
2024-05-07 14:07:36 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+    let newHeader = document.createElement("h1");
+    newHeader.id = "victory";
+    newHeader.innerHTML = "JOEL is the champion"
